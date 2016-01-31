696c6ee88197cf5f789fb3621b7777f09e8e65f0
CuByte <admin@cubyte.net>
2016-01-31 23:33:58 +0100

    bla

--- remove_package.sh
+++ remove_package.sh
@@ -3,7 +3,7 @@
 repo="$(dirname "$(readlink -f "$0")")/repo"
 package="$1"
 
-repo-remove --sign --verify "${repo}/cubyte.db.tar.gz"    "$package"
-repo-remove --sign --verify "${repo}/cubyte.files.tar.gz" "$package"
-rm "${repo}/${package}-*.pkg.tar.*"
+repo-remove --verify --sign "${repo}/cubyte.db.tar.gz"    "$package"
+repo-remove --verify --sign "${repo}/cubyte.files.tar.gz" "$package"
+rm "${repo}/${package}-"*.pkg.tar.*
 
